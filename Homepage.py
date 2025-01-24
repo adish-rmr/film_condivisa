e5cc6462ee35344b41dd8f12f6c2b2fb8533992e
adish-rmr <168539641+adish-rmr@users.noreply.github.com>
2025-01-24 22:32:46 +0100

    update

--- Homepage.py
+++ Homepage.py
@@ -1,73 +1,73 @@
 import streamlit as st
 from class_methods import Film, Catalogue
 from info import get_movie_info, display_info
-import os, pickle
-import random
 import datetime as dt
 
 st.title('Film: La :red[Condivisa]')
 
 userbase = ['reactorr', 'Joy', 'Pietro', 'Luca C.', 'Tiziana', 'Luca I.', 'Carmine']
-
 select = st.selectbox('Select user:', userbase)
 
 if select:
     st.write(f"Welcome back, {select}!")
-
     catalogue = Catalogue()
-    if os.path.exists('films.pkl'):
-        catalogue.import_data()
 
     # MOVIE OF THE WEEK
-
     st.header('Film della settimana')
     check = catalogue.check_film()
-    if check:
-        catalogue.save_data()
-    flag = False
-    for film in catalogue.films:
-        if film.week_f:
-            display_info(film.info)
-            st.write(f"Proposed by: *{film.proposed_by}*")
-            st.write(f"Starts on: {film.start.strftime('%d/%m/%Y')}")
-            st.write(f"Ends on: {film.end.strftime('%d/%m/%Y')}")
-            flag = True
-    if not flag:
+    
+    # Get current week's film
+    current_films = [Film.from_dict(f) for f in catalogue.collection.find({'week_f': True})]
+    
+    if current_films:
+        film = current_films[0]
+        display_info(film.info)
+        st.write(f"Proposed by: *{film.proposed_by}*")
+        st.write(f"Starts on: {film.start.strftime('%d/%m/%Y')}")
+        st.write(f"Ends on: {film.end.strftime('%d/%m/%Y')}")
+    else:
         if st.button('Pick the random film of the week'):
             film = catalogue.random_film()
-            st.success('Film of the week picked!')
-            catalogue.save_data()
+            if film:
+                st.success('Film of the week picked!')
 
     # FILMS LIST
-
     st.subheader('Film proposti')
-    for film in catalogue.films:
-        if not film.week_f and not film.watched:
-            display_info(film.info)
-            st.write(f"Proposed by: *{film.proposed_by}*")
+    available_films = [Film.from_dict(f) for f in 
+                      catalogue.collection.find({'week_f': False, 'watched': False})]
+    
+    for film in available_films:
+        display_info(film.info)
+        st.write(f"Proposed by: *{film.proposed_by}*")
 
     # ADD FILM
-
     st.subheader('Propose a film')
     u_input = st.text_input('Film title:')
+    
     if u_input:
         titolo = get_movie_info(u_input)
         display_info(titolo)
         check = False
-    if st.button('Search'):
-        for film in catalogue.films:
-            if film.info['title'] == titolo['title']:
-                check = True
+
+        if st.button('Search'):
+            # Check if film already exists
+            existing_film = catalogue.collection.find_one({'info.title': titolo['title']})
+            if existing_film:
                 st.error('Film already proposed!')
-            elif film.proposed_by == select and not film.watched:
                 check = True
-                st.error('You have already proposed a film!')
-        if not check or catalogue.films == []:
-            film = Film(titolo, select)
-            catalogue.add_film(film)
-            st.success('Film proposed!')
-            catalogue.save_data()
             
-    
+            # Check if user already proposed an unwatched film
+            user_proposal = catalogue.collection.find_one({
+                'proposed_by': select,
+                'watched': False
+            })
+            if user_proposal:
+                st.error('You have already proposed a film!')
+                check = True
+            
+            if not check:
+                film = Film(titolo, select)
+                catalogue.add_film(film)
+                st.success('Film proposed!')
 
-
+    catalogue.close()
--- class_methods.py
+++ class_methods.py
@@ -1,62 +1,119 @@
-
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import random
 
 class Film:
-    def __init__ (self, info, proposed_by):
+    def __init__(self, info, proposed_by):
         self.info = info
         self.week_f = False
-        self.start = 0
-        self.end = 0
+        self.start = None
+        self.end = None
         self.ratings = {}
         self.proposed_by = proposed_by
         self.special = False
         self.watched = False
     
-    def __str__ (self):
+    def __str__(self):
         return self.info
     
-    def create_rating (self, user, rating):
+    def to_dict(self):
+        return {
+            'info': self.info,
+            'week_f': self.week_f,
+            'start': self.start,
+            'end': self.end,
+            'ratings': self.ratings,
+            'proposed_by': self.proposed_by,
+            'special': self.special,
+            'watched': self.watched
+        }
+    
+    @classmethod
+    def from_dict(cls, data):
+        film = cls(data['info'], data['proposed_by'])
+        film.week_f = data['week_f']
+        film.start = data['start']
+        film.end = data['end']
+        film.ratings = data['ratings']
+        film.special = data['special']
+        film.watched = data['watched']
+        return film
+    
+    def create_rating(self, user, rating):
         self.ratings[user] = rating
-
-    def set_special (self):
+    
+    def set_special(self):
         self.special = True
 
-
 class Catalogue:
-    def __init__ (self):
-        self.films = []
-
-    def add_film (self, film):
+    def __init__(self, mongodb_uri="mongodb+srv://user:So6fB5dMOkCcQiTl@cluster0.mxnrotu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"):
+        self.client = MongoClient(mongodb_uri)
+        self.db = self.client['film_catalogue']
+        self.collection = self.db['films']
+    
+    def add_film(self, film):
         if isinstance(film, Film):
-            self.films.append(film)
+            self.collection.insert_one(film.to_dict())
     
-    def random_film (self):
-        import random
-        import datetime as dt
+    def random_film(self):
+        pipeline = [
+            {'$match': {'week_f': False, 'watched': False}},
+            {'$sample': {'size': 1}}
+        ]
+        film_doc = list(self.collection.aggregate(pipeline))
+        
+        if not film_doc:
+            return None
+            
+        film = Film.from_dict(film_doc[0])
+        film.week_f = True
+        film.start = datetime.now()
+        film.end = datetime.now() + timedelta(days=7)
+        
+        self.collection.update_one(
+            {'info': film.info},
+            {'$set': {
+                'week_f': True,
+                'start': film.start,
+                'end': film.end
+            }}
+        )
+        return film
+    
+    def check_film(self):
+        result = self.collection.update_many(
+            {
+                'week_f': True,
+                'end': {'$lt': datetime.now()}
+            },
+            {
+                '$set': {
+                    'week_f': False,
+                    'watched': True
+                }
+            }
+        )
+        return result.modified_count > 0
+    
+    def get_all_films(self):
+        return [Film.from_dict(doc) for doc in self.collection.find()]
+    
+    def close(self):
+        self.client.close()
 
-        index = random.randint(0, len(self.films) - 1)
-        self.films[index].week_f = True
-        self.films[index].start = dt.datetime.today()
-        self.films[index].end = dt.datetime.today() + dt.timedelta(days=7)
-        return self.films[index]
+# Usage example:
+if __name__ == "__main__":
+    catalogue = Catalogue()
     
-    def check_film (self):
-        import datetime as dt
-        for film in self.films:
-            if film.week_f and film.end < dt.datetime.today():
-                film.week_f = False
-                film.watched = True
-                return True
+    # Add a film
+    new_film = Film("The Matrix", "John")
+    catalogue.add_film(new_film)
     
-    def save_data(self):
-        import pickle
-        with open('films.pkl', 'wb') as f:
-            pickle.dump(self.films, f)
+    # Get random film
+    weekly_film = catalogue.random_film()
     
-    def import_data(self):
-        import pickle
-        with open('films.pkl', 'rb') as f:
-            self.films = pickle.load(f)
+    # Check expired films
+    catalogue.check_film()
     
-
-
-    
+    # Clean up
+    catalogue.close()
